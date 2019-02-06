6892e1bf8793289ffd20e120a8997b424de99e54
vdanilau <47238767+vdanilau@users.noreply.github.com>
2019-02-06 18:05:01 +0300

    Create Decription.ps1

---
+++ Decription.ps1
@@ -0,0 +1,97 @@
+﻿$a='abcdefghijklmnopqrstuvwxyz .' #задали изначальный массив
+
+$b = ''
+$res=''
+
+for ($n=0; $n -lt 24 ; $n++) {  $b+=$a[$n+2] }
+$b =$b + $a[0] + $a[1] + ' ' + '.'
+#$b
+#$b #получили массив-ключ
+
+$m='g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'  #crypto array
+
+
+
+for ($i=0; $i -lt 29 ; $i++) {switch ($m[$i])   
+
+
+
+                                        
+                                       {     $a[0] { $b[0]; break}  
+
+                                             $a[1] {$b[1]; break}  
+                                             $a[2] {$b[2]; break}  
+                                             $a[3] {$b[3]; break}  
+                                             $a[4] {$b[4]; break}  
+                                             $a[5] {$b[5]; break}  
+                                             $a[6] { $b[6] ; break}  
+                                             $a[7] {$b[7]; break}  
+                                             $a[8] {$b[8]; break}  
+                                             $a[9] {$b[9]; break}  
+                                             $a[10] {$b[10]; break}  
+                                             $a[11] {$b[11]; break}  
+                                             $a[12] {$b[12]; break}  
+                                             $a[13] {$b[13]; break}  
+                                             $a[14] {$b[14]; break}  
+                                             $a[15] {$b[15]; break}  
+                                             $a[16] {$b[16]; break}  
+                                             $a[17] {$b[17]; break}  
+                                             $a[18] {$b[18]; break}  
+                                             $a[19] {$b[19]; break}  
+                                             $a[20] {$b[20]; break}  
+                                             $a[21] {$b[21]; break}  
+                                             $a[22] {$b[22]; break}  
+                                             $a[23] {$b[23]; break}  
+                                             $a[24] {$b[24]; break}  
+                                             $a[25] {$b[25]; break}  
+                                             $a[26] {$b[26]; break} 
+                                             $a[27] {$b[27]; break}                                            
+ 
+  }}
+
+
+
+
+
+
+
+ <# foreach ($i in $m)  { switch ('$i')   
+
+
+
+                                        
+                                       {     $a[0] {$res+= $b[0]}  
+
+                                             $a[1] {$b[1]}  
+                                             $a[2] {$res+=$b[2]}  
+                                             $a[3] {$res+=$b[3]}  
+                                             $a[4] {$res+=$b[4]}  
+                                             $a[5] {$res+=$b[5]}  
+                                             $a[6] {" $b[6]" ; break}  
+                                             $a[7] {$res+=$b[7]}  
+                                             $a[8] {$res+=$b[8]}  
+                                             $a[9] {$res+=$b[9]}  
+                                             $a[10] {$res+=$b[10]}  
+                                             $a[11] {$res+=$b[11]}  
+                                             $a[12] {$res+=$b[12]}  
+                                             $a[13] {$res+=$b[14]}  
+                                             $a[15] {$res+=$b[15]}  
+                                             $a[16] {$res+=$b[16]}  
+                                             $a[17] {$res+=$b[17]}  
+                                             $a[18] {$res+=$b[18]}  
+                                             $a[19] {$res+=$b[19]}  
+                                             $a[20] {$res+=$b[20]}  
+                                             $a[21] {$res+=$b[21]}  
+                                             $a[22] {$res+=$b[22]}  
+                                             $a[23] {$res+=$b[23]}  
+                                             $a[24] {$res+=$b[24]}  
+                                             $a[25] {$res+=$b[25]}  
+                                             $a[26] {$res+=$b[26]}  
+                                             $a[27] {$res+=$b[27]} 
+                                             $a[28] {$res+=$b[28]}                                            
+ 
+ 
+  }} #>
+ 
+
+
